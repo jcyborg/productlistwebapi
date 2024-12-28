1475751d799963f724dd0cbe618ee7f2383d666a
Jubril Bakare <jubril@thecodebridge.com>
2024-12-28 03:39:06 -0500

    prd

--- Program.cs
+++ Program.cs
@@ -13,7 +13,7 @@ builder.Services.AddCors(options =>
 {
     options.AddDefaultPolicy(policy =>
     {
-        policy.WithOrigins("https://white-stone-0194ad60f.4.azurestaticapps.net/") 
+        policy.WithOrigins("https://white-stone-0194ad60f.4.azurestaticapps.net") 
               .AllowAnyHeader()
               .AllowAnyMethod();
     });
